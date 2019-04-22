bf201524ff06ba28dbcd7f2d5a60d9bf883f7905
qiuxk <qiuxk@example.com>
2019-04-22 17:49:06 +0800

    init

---
+++ Example/Pods/Target Support Files/KSToolsGroup/KSToolsGroup-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KSToolsGroup : NSObject
+@end
+@implementation PodsDummy_KSToolsGroup
+@end
---
+++ Example/Pods/Target Support Files/Pods-KSToolsGroup_Example/Pods-KSToolsGroup_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KSToolsGroup_Example : NSObject
+@end
+@implementation PodsDummy_Pods_KSToolsGroup_Example
+@end
