f9b3b5e4a4cdc721fa1337e69e4112d652b6f050
qiuxk <qiuxk@example.com>
2019-05-14 14:49:35 +0800

    0.1.12

--- Example/KSToolsGroup/ViewController.swift
+++ Example/KSToolsGroup/ViewController.swift
@@ -15,18 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         textfield.inputAccessoryView = KSToolbar(controller:self)
         // Do any additional setup after loading the view, typically from a nib.
-        let tableV = KSTable(frame: CGRect(x: 0,y: 50,width: 300,height: 500),identifier:String(describing: TicketSummaryCell.self))
-        tableV.ds = [["java","swift","js"],["java","swift","js"]]
-        tableV.onCellData = { cell,obj in
-            let acell = cell as! TicketSummaryCell
-            acell.lab1.text = ""
-            acell.lab2.text = ""
-            acell.lab3.text = ""
-            acell.lab4.text = obj as? String
-        }
-        self.view.addSubview(tableV)
-   
-       
+        KSToast("9999")
     }
 
     override func didReceiveMemoryWarning() {
